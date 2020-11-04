ae1ca4d41d1d17d4da059ef3e6730f88de20b5fe
KJA <31474178+kimjand@users.noreply.github.com>
2020-11-04 21:49:46 +0100

    Add named placeholders to PgPreparedStatement. This allows fewer parameters to be send to the backend when parameters are used multiple times.
    Signed-off-by: KJA <31474178+kimjand@users.noreply.github.com>

--- pgjdbc/src/test/java/org/postgresql/jdbc/NamedParametersTest.java
+++ pgjdbc/src/test/java/org/postgresql/jdbc/NamedParametersTest.java
@@ -21,8 +21,6 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class NamedParametersTest extends BaseTest4 {
 
