f226df9c23944734b9db327f75f0fa225529c1a6
KJA <31474178+kimjand@users.noreply.github.com>
2020-11-11 20:01:53 +0100

    Add named placeholders to PgPreparedStatement. This allows fewer parameters to be send to the backend when parameters are used multiple times.
    Signed-off-by: KJA <31474178+kimjand@users.noreply.github.com>

--- benchmarks/src/jmh/java/org/postgresql/benchmark/statement/ParseStatement.java
+++ benchmarks/src/jmh/java/org/postgresql/benchmark/statement/ParseStatement.java
@@ -30,7 +30,6 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
