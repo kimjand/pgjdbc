baa5cf109de3a592f5699ce4d6034afbefa87c55
KJA <31474178+kimjand@users.noreply.github.com>
2021-06-27 23:01:28 +0200

    Add named placeholders to PgPreparedStatement. Make checker happy broke the error message.
    Signed-off-by: KJA <31474178+kimjand@users.noreply.github.com>

--- pgjdbc/src/main/java/org/postgresql/core/Parser.java
+++ pgjdbc/src/main/java/org/postgresql/core/Parser.java
@@ -21,7 +21,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
