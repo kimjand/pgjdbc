94cecc7daa082a92766fddd68b66fad5db2721ea
KJA <31474178+kimjand@users.noreply.github.com>
2022-04-29 17:06:58 +0200

    Add named placeholders to PgPreparedStatement. Correct copyright year.
    Signed-off-by: KJA <31474178+kimjand@users.noreply.github.com>

--- pgjdbc/src/main/java/org/postgresql/jdbc/PlaceholderStyles.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PlaceholderStyles.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, PostgreSQL Global Development Group
+ * Copyright (c) 2022, PostgreSQL Global Development Group
  * See the LICENSE file in the project root for more information.
  */
 
